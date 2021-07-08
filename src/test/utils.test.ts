486291c7dd706d4087762a6b79cedf6983066dbc
rebornix <penn.lv@gmail.com>
2021-07-08 11:13:18 -0700

    add tests for isEqual

---
+++ src/test/utils.test.ts
@@ -0,0 +1,16 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import 'mocha';
+import * as assert from 'assert';
+import { isEqual } from '../common/utils';
+
+suite('Utils', () => {
+	test('isEqual', () => {
+        assert.ok(isEqual(['a'], ['a']))
+        assert.ok(!isEqual(['a'], ['b']));
+        assert.ok(!isEqual(['a', 'b'], ['b', 'a']));
+    });
+});
