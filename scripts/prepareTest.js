2aaa4be513408c80d26a63562f930d2189c01d68
Rich Chiodo <rchiodo@users.noreply.github.com>
2021-09-14 16:51:43 -0700

    Beginnings of potential solution

--- scripts/prepareTest.js
+++ scripts/prepareTest.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 
+fs.mkdirSync('./out');
+fs.mkdirSync('./out/test');
 fs.copyFile('./src/test/package.json', './out/test/package.json', (err) => {
   if (err) throw err;
   console.log('src/test/package.json was copied to out/test/package.json');
--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,23 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import { Disposable, DocumentSelector } from 'vscode';
+import { Disposable, DocumentSelector, Uri } from 'vscode';
 import { LanguageClient, Middleware } from 'vscode-languageclient/node';
 
 import { IVSCodeNotebook } from './common/types';
 import { ConsumingMiddlewareAddon } from './consumingMiddlewareAddon';
 import { NotebookMiddlewareAddon } from './notebookMiddlewareAddon';
 
-export type MiddlewareAddon = Middleware & Disposable;
-
-// Factory method for creating the middleware addon
-export function createMiddlewareAddon(
+// Factory method for creating the middleware
+export function createNotebookMiddleware(
     notebookApi: IVSCodeNotebook,
     getClient: () => LanguageClient | undefined,
     traceInfo: (...args: any[]) => void,
     cellSelector: DocumentSelector,
-    notebookFileRegex: RegExp,
-    pythonPath?: string,
-    trace?: (message: string) => void
-): MiddlewareAddon {
-    // We have two types of middleware addons.
-    // - Python based addon that python extension creates. It consumes
-    // all events for python selector
-    // - Notebook based addon taht jupyter extension creates. It processes
-    // events for a jupyter (python) notebook
-    if (
-        (typeof cellSelector === 'string' && cellSelector === 'python') ||
-        (Array.isArray(cellSelector) && cellSelector.find((s) => s === 'python' || s.language === 'python'))
-    ) {
-        return new ConsumingMiddlewareAddon();
-    } else {
-        return new NotebookMiddlewareAddon(
-            notebookApi,
-            getClient,
-            traceInfo,
-            cellSelector,
-            notebookFileRegex,
-            pythonPath,
-            trace
-        );
-    }
+    pythonPath: string,
+    shouldProvideIntellisense: (uri: Uri) => boolean
+): Middleware & Disposable {
+    // Current idea:
+    // LanguageClients are created per interpreter (as they start) with a selector for all notebooks
+    // Middleware swallows all requests for notebooks that don't match itself (shouldProvideIntellisense returns false)
+    // Python extension is modified to no longer do intellisense for notebooks or interactive window
 }
